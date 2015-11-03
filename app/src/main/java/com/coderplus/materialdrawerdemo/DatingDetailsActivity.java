3d73d86356fd829ee5dcffc383b4080a88ab9209
xiejianchao <xiejc01@gmail.com>
2015-11-03 18:42:15 +0800

    约会详情

--- app/src/main/java/com/coderplus/materialdrawerdemo/DatingDetailsActivity.java
+++ app/src/main/java/com/coderplus/materialdrawerdemo/DatingDetailsActivity.java
@@ -5,7 +5,9 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.InputType;
 import android.view.View;
+import android.widget.EditText;
 
 import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
@@ -24,6 +26,9 @@ public class DatingDetailsActivity extends BaseActivity {
     public void init(Bundle savedInstanceStat) {
         ViewUtils.inject(this);
         setSupportActionBar(toolbar);
+
+        EditText edit=(EditText)findViewById(R.id.et_content1);
+//        edit.setInputType(InputType.TYPE_NULL);
 
     }
 
